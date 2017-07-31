43d2801fa9cdfc1e822727b7f4c6408b13309839
Ethan <liao1ping@gmail.com>
2017-07-31 17:09:19 -0500

    trying to upload to aws

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 POSTGRES = {
     'db': 'ers-psql.chwc2d4rnuv3.us-east-2.rds.amazonaws.com:5432',
     'host': 'localhost',
-    'user': 'erspsql',
+    'user': 'postgres',
     'pw': 'ethanrobertsarah',
     'port': '5432',
 }
